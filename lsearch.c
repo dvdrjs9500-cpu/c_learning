8374616d8864899f4a88bd2c5befaa2e0de609bf
dvdrjs9500-cpu <dvdrjs9500@gmail.com>
2026-03-04 14:41:21 +0530

    Linear search

---
+++ lsearch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n, i, key, found = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    scanf("%d", &key);
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = 1;
+            break;
+        }
+    }
+    if(found)
+        printf("Element Found");
+    else
+        printf("Not Found");
+    return 0;
+}
